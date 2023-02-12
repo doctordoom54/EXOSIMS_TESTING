9ed0b152c3bf15ddb2bf8d5c21e2fb92ccca4f98
Sachin Kelkar <ssk242@cornell.edu>
2023-02-11 19:35:00 -0500

    Flowchart update

--- koMap.py
+++ koMap.py
@@ -1,19 +1,18 @@
 import numpy as np
+
 i = 0
-a = np.random.randint(10,size = (5,5))
+a = np.random.randint(10, size=(5, 5))
 A = 1
 counter = 0
 while A == 1:
-    h = np.unravel_index(a.argmin(),a.shape)
-    t = np.random.choice([1,1,0,0,1,1,1,1,1,1,0,1])
-    y = np.random.choice([1,1,1,1,1,0,1,1,1,0,1,1,0,1])
+    h = np.unravel_index(a.argmin(), a.shape)
+    t = np.random.choice([1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1])
+    y = np.random.choice([1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1])
     counter = counter + 1
     print(t)
     print(y)
-    A = t+y 
-    if A == 0 :
-        print(A)
-        print(counter)
+    A = t + y
+    if A == 0:
         pass
     else:
         print(counter)
--- test_multiSS.py
+++ test_multiSS.py
@@ -1,3 +1,4 @@
 import EXOSIMS,EXOSIMS.MissionSim,os.path
 scriptfile = os.path.join(EXOSIMS.__path__[0],'Scripts','multiOcculterScript_test.json')
 sim = EXOSIMS.MissionSim.MissionSim(scriptfile)
+"""sim.run_sim()"""
